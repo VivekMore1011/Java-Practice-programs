c4000fd5e7774a82db4e24f7510e4ef4778f7644
VivekMore1011 <vivekmore1011@gmail.com>
2026-09-18 16:45:00 +0530

    new programs

---
+++ MapCollection.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class MapCollection{
+    public static void main(String[] args) {
+        Map<Integer,String>students=new HashMap<>();
+
+        students.put(101,"Vivek");
+        students.put(102,"Vidya");
+
+        System.out.println(students);
+    }
+}
